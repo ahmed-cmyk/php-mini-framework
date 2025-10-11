7b4a864bebe9c36708429102c8b675d3fbcf5b29
Ahmed Ikram <ikramahmed398@gmail.com>
2025-10-11 08:02:36 +0500

    feat: updated config files

---
+++ config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => env('APP_NAME', 'My PHP App'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => 'UTC',
+    'locale' => 'en',
+];
--- config/db.php
+++ config/db.php
@@ -1,8 +1,18 @@
 <?php
 
 return [
-    'host' => 'localhost',
-    'port' => 3306,
-    'dbname' => 'blog',
-    'charset' => 'utf8mb4'
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'myapp'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+        ],
+    ],
 ];
