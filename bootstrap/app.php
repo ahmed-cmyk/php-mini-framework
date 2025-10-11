001852c7555ab17a56969ee17d43e6ad43e5ef61
Ahmed Ikram <ikramahmed398@gmail.com>
2025-10-11 16:42:42 +0500

    feat: .env is generated automatically from .env.example if it does not exist

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,15 +13,20 @@ $app = new Container();
 // 2. Set Facade application container
 \App\Support\Facade::setFacadeApplication($app);
 
-// 3. Setup environment variables
+// 3. Create .env file based on .env.example if it doesn't exist
+if (!file_exists(__DIR__. '/../.env' && file_exists(__DIR__ . '/../.env.example'))) {
+    copy(__DIR__ . '/../.env.example', __DIR__ . '/../.env');
+}
+
+// 4. Setup environment variables
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-// 4. Register provider
+// 5. Register provider
 $provider = new AppServiceProvider($app);
 $provider->register();
 
-// Boot RouteServiceProvider to load routes
+// 6. Boot RouteServiceProvider to load routes
 if (method_exists($provider, 'registerRouteServiceProvider')) {
 	$provider->registerRouteServiceProvider();
 }
